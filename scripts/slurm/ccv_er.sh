024a40c1420af8637ab62bc1bb7019d38680ca9e
Mathias Skavhaug <mathias@skavhaug.eu>
2022-03-09 22:07:24 +0100

    Update slurm scripts

--- scripts/slurm/ccv_er.sh
+++ scripts/slurm/ccv_er.sh
@@ -2,8 +2,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuA100 
 #SBATCH --time=01:30:00
-#SBATCH --job-name=ccv_pls
-#SBATCH --output=ccv_pls.out
+#SBATCH --job-name=ccv_er
+#SBATCH --output=ccv_er.out
  
 # Activate environment
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
@@ -12,7 +12,7 @@ uenv miniconda3-py38
 conda activate ccv
 python longchecker/longchecker/predict.py \
     --checkpoint_path="longchecker/checkpoints/scifact.ckpt" \
-    --input_file="data/predict_claims.jsonl" \
-    --corpus_file="data/predict_corpus.jsonl" \
-    --output_file="data/predict_result.jsonl" \
+    --input_file="data/eclaims.jsonl" \
+    --corpus_file="data/ecorpus.jsonl" \
+    --output_file="data/erelations.jsonl" \
     --batch_size 16
