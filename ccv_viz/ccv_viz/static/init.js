a6fb5065c4689776eada701ed572bbc078fb3ee6
Mathias Skavhaug <mathias@skavhaug.eu>
2022-03-16 18:25:11 +0100

    Fix formatting

--- ccv_viz/ccv_viz/static/init.js
+++ ccv_viz/ccv_viz/static/init.js
@@ -3,12 +3,12 @@ export function init(graph) {
         height = d3.select("#graph-container").node().getBoundingClientRect().height;
 
     var viz = d3.select("svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
+            .attr("width", "100%")
+            .attr("height", "100%")
         .append("g")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("id", "viz")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("id", "viz")
 
     var node_color = d3.scaleOrdinal(d3.schemeDark2);
     var link_color = d3.scaleOrdinal(["#b81809", "#09bd1b", "#cf620e"]).domain(["0", "1", "2"]).unknown("#999");
@@ -23,32 +23,34 @@ export function init(graph) {
         .force("center", d3.forceCenter(width / 2, height / 2));
 
     var link = viz.append("g")
-        .attr("class", "links")
+            .attr("class", "links")
         .selectAll("line")
-        .data(graph.links)
-        .enter().append("line")
-        .classed("link", true)
-        .attr("stroke-width", function (d) {
-            return d.value;
-        })
-        .attr("stroke", function (d) {
-            return link_color(d.label)
-        });
+            .data(graph.links)
+            .enter()
+        .append("line")
+            .classed("link", true)
+            .attr("stroke-width", function (d) {
+                return d.value;
+            })
+            .attr("stroke", function (d) {
+                return link_color(d.label)
+            });
 
     var node = viz.append("g")
-        .attr("class", "nodes")
+            .attr("class", "nodes")
         .selectAll("circle")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .classed("node", true)
-        .attr("r", 5)
-        .attr("fill", function (d) {
-            return node_color(d.group);
-        })
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+            .data(graph.nodes)
+            .enter()
+        .append("circle")
+            .classed("node", true)
+            .attr("r", 5)
+            .attr("fill", function (d) {
+                return node_color(d.group);
+            })
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
 
     node.append("title")
         .text(function (d) {
@@ -64,26 +66,14 @@ export function init(graph) {
 
     function ticked() {
         link
-            .attr("x1", function (d) {
-                return d.source.x;
-            })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
-            });
+            .attr("x1", function (d) {return d.source.x;})
+            .attr("y1", function (d) {return d.source.y;})
+            .attr("x2", function (d) {return d.target.x;})
+            .attr("y2", function (d) {return d.target.y;});
 
         node
-            .attr("cx", function (d) {
-                return d.x;
-            })
-            .attr("cy", function (d) {
-                return d.y;
-            });
+            .attr("cx", function (d) {return d.x;})
+            .attr("cy", function (d) {return d.y;});
     }
 
     function dragstarted(d) {
