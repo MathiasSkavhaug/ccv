c3d9378699d0ad142d765703e9fc8865f1301fb1
Mathias Skavhaug <mathias@skavhaug.eu>
2022-03-16 14:07:10 +0100

    Change indent from 2 spaces to 4 spaces

--- ccv_viz/ccv_viz/static/resize.js
+++ ccv_viz/ccv_viz/static/resize.js
@@ -1,40 +1,44 @@
 export function resize() {
-  var viz = d3.select("#viz")
-  var zoom = d3.zoom()
-    .on("zoom", zoomed);
+    var viz = d3.select("#viz");
+    var zoom = d3.zoom().on("zoom", zoomed);
 
-  function zoomed() {
-    viz.attr("transform", d3.event.transform);
-  }
+    function zoomed() {
+        viz.attr("transform", d3.event.transform);
+    }
 
-  function zoomFit(transitionDuration) {
-    var fullWidth = $("#graph-container").width(),
-      fullHeight = $("#graph-container").height()
-    var vizBounds = viz.node().getBBox()
-    var centerX = vizBounds.x + vizBounds.width / 2,
-      centerY = vizBounds.y + vizBounds.height / 2;
-    var scale = 0.80 / Math.max(vizBounds.width / fullWidth, vizBounds.height / fullHeight);
-    var translate = [fullWidth / 2 - scale * centerX, fullHeight / 2 - scale * centerY]
+    function zoomFit(transitionDuration) {
+        var fullWidth = $("#graph-container").width(),
+            fullHeight = $("#graph-container").height();
+        var vizBounds = viz.node().getBBox();
+        var centerX = vizBounds.x + vizBounds.width / 2,
+            centerY = vizBounds.y + vizBounds.height / 2;
+        var scale =
+            0.8 /
+            Math.max(vizBounds.width / fullWidth, vizBounds.height / fullHeight);
+        var translate = [
+            fullWidth / 2 - scale * centerX,
+            fullHeight / 2 - scale * centerY,
+        ];
 
-    viz
-      .transition()
-      .duration(transitionDuration || 0)
-      .call(zoom.transform,
-        d3.zoomIdentity
-        .translate(translate[0], translate[1])
-        .scale(scale));
-  }
+        viz
+            .transition()
+            .duration(transitionDuration || 0)
+            .call(
+                zoom.transform,
+                d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale)
+            );
+    }
 
-  function resizeGraph() {
-    // update viz size.
-    var width = $("#graph-container").width(),
-      height = $("#graph-container").height()
-    viz.attr("width", width).attr("height", height);
+    function resizeGraph() {
+        // update viz size.
+        var width = $("#graph-container").width(),
+            height = $("#graph-container").height();
+        viz.attr("width", width).attr("height", height);
 
-    // center and scale the network graph
-    zoomFit(1000);
-  }
+        // center and scale the network graph
+        zoomFit(1000);
+    }
 
-  window.addEventListener('resize', resizeGraph);
-  setTimeout(resizeGraph, 400);
-};
+    window.addEventListener("resize", resizeGraph);
+    setTimeout(resizeGraph, 400);
+}
