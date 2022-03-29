61cadec2323b5f751bcd93e8223548b68954e36d
Mathias Skavhaug <mathias@skavhaug.eu>
2022-03-29 18:20:35 +0200

    Add option to color nodes according to publish date

--- ccv_viz/ccv_viz/static/scripts/graphOptionsBar.js
+++ ccv_viz/ccv_viz/static/scripts/graphOptionsBar.js
@@ -1,6 +1,7 @@
 import { init } from "./main.js"
 import { resetGraph } from "./util.js"
 import { runSRWR } from "./graphSRWR.js"
+import { getNeighborsOfType, getNodesWithIds } from "./graphTraversal.js";
 
 export function graphOptionsBarInit(graph) {
     d3.select("#options-arrow")
@@ -25,9 +26,21 @@ export function graphOptionsBarInit(graph) {
             d3.select(this).classed("option-selected", !active)
         });
 
+    d3.select("#option-date")
+        .on("click", function() {
+            var active = d3.select(this).classed("option-selected")
+            if (active) {
+                changeFromDateColors();
+            } else {
+                changeToDateColors();
+            }
+            d3.select(this).classed("option-selected", !active)
+        });
+
     // Add tooltip to option buttons.
-    d3.select("#option-author").append("title").text("Toggle authors")
-    d3.select("#option-algorithm").append("title").text("Toggle SRWR")
+    d3.select("#option-author").append("title").text("Toggle authors");
+    d3.select("#option-algorithm").append("title").text("Toggle SRWR");
+    d3.select("#option-date").append("title").text("Toggle publish date colors");
 };
 
 // Hides or shows the options bar, depending on current state.
@@ -59,4 +72,94 @@ function toggleAuthors(graph) {
     } else {
         init(graph, ["author"])
     }
+}
+
+// Changes node colors to reflect their date and adds a color bar to the graph.
+function changeToDateColors() {
+    var colors = ["#a1cbe3","#08306b"];
+
+    // Define time scale.
+    var dates = d3.selectAll(".node.document").data().map(function(d) { return d3.timeParse("%Y-%m-%d")(d.date) })
+    var range = d3.scaleTime().domain([d3.min(dates), d3.max(dates)]).range(colors);
+
+    // Color nodes according to their date.
+    d3.selectAll(".node.document")
+        .style("fill", function(d) {return range(d3.timeParse("%Y-%m-%d")(d.date))})
+        .each(function(doc) {
+            getNodesWithIds(getNeighborsOfType(doc, "evidence").data().map(function(evi) {return evi.id}))
+                .style("fill", d3.select(this).style("fill"))
+            getNodesWithIds(getNeighborsOfType(doc, "author").data().map(function(evi) {return evi.id}))
+                .style("fill", d3.select(this).style("fill"))
+        });
+
+    addColorBar(colors, d3.min(dates), d3.max(dates));
+};
+
+// Adds the color bar to the graph.
+function addColorBar(colors, minDate, maxDate) {
+    var container = d3.select("#graph-svg")
+        .append("g")
+            .attr("id", "color-bar")
+
+    var gradient = container
+        .append("linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", "0%")
+        .attr("x2", "100%")
+        .attr("y1", "0%")
+        .attr("y2", "0%")
+
+    gradient.selectAll("stop")
+            .data(colors)
+            .enter()
+        .append("stop")
+            .style("stop-color", function(d) { return d })
+            .attr("offset", function(d,i) {
+                return 100 * (i / (colors.length - 1)) + "%";
+            });
+
+    var barCenter = 50;
+    var barWidth = 25;
+    var barHeight = 2;
+
+    container
+        .append("rect")
+            .attr("id", "color-bar-rect")
+            .attr("x", barCenter - barWidth/2 + "%")
+            .attr("y", 100 - 1 - barHeight + "%")
+            .attr("width", barWidth + "%")
+            .attr("height", barHeight + "%")
+            .style("fill", "url(#gradient)");
+
+    container
+        .append("text")
+            .classed("color-bar-text", true)
+            .classed("color-bar-text-left", true)
+            .attr("x", barCenter - 1 - barWidth/2 + "%")
+            .attr("y", 100 - 1.5 + "%")
+            .attr("height", barHeight + "%")
+            .text(d3.timeFormat("%Y-%m-%d")(minDate))
+
+    container
+        .append("text")
+            .classed("color-bar-text", true)
+            .attr("x", barCenter + 1 + barWidth/2 + "%")
+            .attr("y", 100 - 1.5 + "%")
+            .attr("height", barHeight + "%")
+            .text(d3.timeFormat("%Y-%m-%d")(maxDate))
+};
+
+// Changes node colors back to normal colors and removes the color bar.
+function changeFromDateColors() {
+    d3.selectAll(".node.document")
+    .style("fill", null)
+    .each(function(doc) {
+        getNodesWithIds(getNeighborsOfType(doc, "evidence").data().map(function(evi) {return evi.id}))
+            .style("fill", null)
+        getNodesWithIds(getNeighborsOfType(doc, "author").data().map(function(evi) {return evi.id}))
+            .style("fill", null)
+    });
+
+
+    d3.select("#color-bar").remove()
 }
